0d47e62fffd2b2c23d524511b1de1b02a773987a
JhonnDevJs <153306997+JhonEG-17@users.noreply.github.com>
2026-02-10 10:33:10 -0600

    fix: delete import navbar & import new components

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 export const version = "0.0.1";
-export * from "./components/SemaButton/SemaButton";
-export * from "./components/SemaAlerts/SemaAlerts";
 export * from "./components/SemaAccordion/SemaAccordion"
-export * from "./components/SemaFaqs/SemaFaqs"
+export * from "./components/SemaAlerts/SemaAlerts";
 export * from "./components/SemaBreadcrumbs/SemaBreadcrumbs"
-export * from "./components/SemaDropdown/SemaDropdown"
+export * from "./components/SemaButton/SemaButton";
+export * from "./components/SemaCardFeature/SemaCardFeature"
+export * from "./components/SemaCardInfo/SemaCardInfo"
 export * from "./components/SemaCardProduct/SemaCardProduct"
+export * from "./components/SemaCodeSnippet/SemaCodeSnippet"
+export * from "./components/SemaDropdown/SemaDropdown"
+export * from "./components/SemaFaqs/SemaFaqs"
